49aa384848f32732041f808ebf00722620162d63
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-04 14:46:06 +0100

    create api project

---
+++ api/app/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+# from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "movies"
+DB_USERNAME = "root"
+DB_PASSWORD = ""
+DB_HOST = "localhost"
+DB_PORT = 3306
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@host:{DB_PORT}/{DB_NAME}'
+
+    db.init_app(app)
+
+    # from .views import views
+    # from .auth import auth
+
+    # app.register_blueprint(views, url_prefix="/")
+    # app.register_blueprint(auth, url_prefix="/")
+
+    # from .models import User, Post, Comment, Like
+
+    # create_database(app)
+
+    # login_manager = LoginManager()
+    # login_manager.login_view = "auth.login"
+    # login_manager.init_app(app)
+
+    # @login_manager.user_loader
+    # def load_user(id):
+    #     return User.query.get(int(id))
+
+    return app
