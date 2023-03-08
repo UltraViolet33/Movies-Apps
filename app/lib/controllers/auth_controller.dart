ee760a540bf67250c039c191591a4c3ff89a3cdb
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-08 15:29:55 +0100

    snackbar is displayed if registration is successfull

--- app/lib/controllers/auth_controller.dart
+++ app/lib/controllers/auth_controller.dart
@@ -16,27 +16,25 @@ class AuthController {
           username.isNotEmpty &&
           password.isNotEmpty &&
           passwordConfirmation.isNotEmpty) {
-            
+
         if (passwordConfirmation != password) {
           Get.snackbar("Error creating Account", "Passwords don't match");
         }
 
-        // http.Response response = await _client.post(_registerUrl, body: {
-        //   "email": email,
-        //   "username": username,
-        //   "password": password,
-        //   "password_confirmation": passwordConfirmation
-        // });
+        http.Response response = await _client.post(_registerUrl, body: {
+          "email": email,
+          "username": username,
+          "password": password,
+          "password_confirmation": passwordConfirmation
+        });
 
-        // print(response.body);
-
-        // if (response.statusCode == 200) {
-        //   Get.snackbar("Account Created", "Welcome !");
-        //   Get.to(LoginScreen());
-        // } else {
-        //   Get.snackbar(
-        //       "Error creating Account", jsonDecode(response.body)["msg"]);
-        // }
+        if (response.statusCode == 200) {
+          Get.snackbar("Account Created", "Welcome !");
+          Get.to(LoginScreen());
+        } else {
+          Get.snackbar(
+              "Error creating Account", jsonDecode(response.body)["msg"]);
+        }
       } else {
         Get.snackbar("Error creating Account", "Please enter all the fields");
       }
