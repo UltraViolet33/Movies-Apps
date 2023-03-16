2c1fe38a4c34c19570ddf66229f19152c03dc668
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-16 11:29:21 +0100

    add Category model

---
+++ api/app/models/Category.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import validates
+from .. import db
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+
+    movies = db.relationship("Movie", backref="movie", passive_deletes=True)
+
+    def __init__(self, name):
+        self.name = name
