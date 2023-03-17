65b3dd881c9fe0aca66fd29dded552b0cf276548
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-17 12:43:46 +0100

    modifs tests unit user model

--- api/tests/unit/test_models.py
+++ api/tests/unit/test_models.py
@@ -2,7 +2,7 @@ import pytest
 from app.models.User import User
 
 
-def test_new_user(test_client):
+def test_new_user(test_client, init_database):
     user = User(email="test1@gmail.com", username="test",
                 password_plaintext="Test123!")
     assert user.email == "test1@gmail.com"
@@ -10,7 +10,7 @@ def test_new_user(test_client):
     assert user.is_password_correct("Test123!") == True
 
 
-def test_raise_exception_for_username(test_client):
+def test_raise_exception_for_username(test_client, init_database):
     with pytest.raises(AssertionError):
         user = User(email="test@gmail.com", username="",
                     password_plaintext="Test123!")
@@ -24,7 +24,7 @@ def test_raise_exception_for_username(test_client):
                     password_plaintext="Test123!")
 
 
-def test_raise_exception_for_email(test_client):
+def test_raise_exception_for_email(test_client, init_database):
     with pytest.raises(AssertionError):
         user = User(email="", username="test", password_plaintext="Test123!")
     with pytest.raises(AssertionError):
