9ccee82f2082c21a3b0f62a5f5f9979bd534e32a
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-04-01 15:03:10 +0200

    create random movies screen

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -1,3 +1,4 @@
+import 'package:app/views/screens/random_movies.dart';
 import 'package:flutter/material.dart';
 // import 'package:tiktok_clone/controllers/auth_controller.dart';
 
@@ -9,7 +10,13 @@ const borderColor = Colors.grey;
 // CONTROLLER
 // var authController = AuthController.instance;
 
-
 // API ENDPOINTS
-
 const apiEndpoint = "http://192.168.1.10:5000";
+
+const pages = [
+  Text("Home screen"),
+  RandomMovies(),
+  Text("Search screen"),
+  Text("Watch List screen"),
+  Text("Random movies screen"),
+];
---
+++ app/lib/views/screens/random_movies.dart
@@ -0,0 +1,43 @@
+import 'package:app/api/movies_api.dart';
+import 'package:app/models/movie.dart';
+import 'package:app/views/widgets/movies_list.dart';
+import 'package:flutter/material.dart';
+
+class RandomMovies extends StatefulWidget {
+  const RandomMovies({Key? key}) : super(key: key);
+
+  @override
+  State<RandomMovies> createState() => _RandomMoviesState();
+}
+
+class _RandomMoviesState extends State<RandomMovies> {
+  late Future<List<Movie>> movies;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = MoviesApi().fetchMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("random movies")),
+      body: Center(
+        child: Expanded(
+          child: Center(
+              child: FutureBuilder<List<Movie>>(
+            future: movies,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) print(snapshot.error);
+
+              return snapshot.hasData
+                  ? MoviesList(movies: snapshot.data)
+                  : const Center(child: CircularProgressIndicator());
+            },
+          )),
+        ),
+      ),
+    );
+  }
+}
